9a2a50d6c9131a92485ac9c6947f5a3ce4e77241
Hayden Sykes <haydensykes13@gmail.com>
2020-02-08 14:35:55 -0500

    add honeypot field callout to the form.

--- src/components/Main.js
+++ src/components/Main.js
@@ -85,7 +85,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" action="/submitted" data-netlify="true" enctype="multipart/form-data">
+          <form name="contact" method="POST" action="/submitted" data-netlify="true" netlify-honeypot="bot-field" enctype="multipart/form-data">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" required/>
@@ -105,7 +105,7 @@ class Main extends React.Component {
               <li>
                 <input type="reset" value="Reset" />
               </li>
-              <input type="hidden" name="hidden-contact" value="contact" />
+              <input type="hidden" name="bot-field" value="contact" />
             </ul>
           </form>
           <ul className="icons">
