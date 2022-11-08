b2fe23b86c58f51ce1162a1891a63662b6b22387
Hayden Sykes <haydensykes13@gmail.com>
2022-11-08 08:52:25 -0500

    landing page completed, ready to push live

---
+++ components/Logo.js
@@ -0,0 +1,20 @@
+import styles from "./logo.module.css";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className={styles.container}>
+      <Image
+        src="/images/HS_logo_glitch.png" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={122} // Desired size with correct aspect ratio
+        alt="Hayden Sykes Logo"
+        className={styles.logoImage}
+      />
+      <h1 className={styles.name}>
+        <span className={styles.h}> H</span>ayden{" "}
+        <span className={styles.s}>S</span>ykes
+      </h1>
+    </div>
+  );
+}
